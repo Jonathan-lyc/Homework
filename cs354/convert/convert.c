cefa87cddef4702fb1ecc0ed30e4adf1330b7c1b
Josh Gachnang <Josh@PCsForEducation.com>
2010-10-06 16:48:14 -0500

    Finished convert.c

--- cs354/convert/convert.c
+++ cs354/convert/convert.c
@@ -1,6 +1,5 @@
 /* Assignment1 Convert
  * by Josh Gachnang
- * CS354, Section 1. Instructer: Karen Miller
  *
  * This program takes up to 6 hexcadecimal numbers and converts them to
  * binary and decimal notation, while checking for errors. It then prints
@@ -11,6 +10,7 @@
 #include "stdio.h"
 
 #define ARRAYSIZE 6
+#define DEBUG 1
 
 
 int parseHex(char hex[]);
@@ -23,10 +23,10 @@ int parseHex(char hex[]) {
     return -1;
   }
   int pos = 2;
-  char curr;  
+  char curr;
   curr = hex[pos];
   int count = 0;
-  
+
   /* Checks all the values after 0x to make sure they are valid, and
    * counts the number of values to make conversions easier. Count + 1
    * will be the last index of array (other than \0)
@@ -34,10 +34,10 @@ int parseHex(char hex[]) {
   while ( curr != '\0' ) {
     pos++;
     int ascii = curr;
-    
+
     /* Acceptable ASCII values are 48-57, and 97 to 102, inclusive. */
     if (ascii < 48 || (ascii > 57 && ascii < 97) || ascii > 102) {
-      return -1; 
+      return -1;
     }
     curr = hex[pos];
     count++;
@@ -58,7 +58,7 @@ int parseHex(char hex[]) {
       offset++;
     }
   return total;
-    
+
 }
 int powerOf(int a, int b) {
   int count = 0;
@@ -68,7 +68,7 @@ int powerOf(int a, int b) {
     return 1;
   }
   if ( b == 1 ) {
-    return a;  
+    return a;
   }
   for ( i = 1; i < b; i++ ) {
     total = total * a;
@@ -88,12 +88,11 @@ void printOutput(int array[], char *copyOfArgv[], int numOfInts) {
   }
 }
 
-/* This function prints an integer (the first parameter) to standard output, 
-in the radix (base) given as the second parameter. The function is expected 
+/* This function prints an integer (the first parameter) to standard output,
+in the radix (base) given as the second parameter. The function is expected
 to wo/* Checks all the values after 0x to make sure they are valid, and
    * counts the number of values to make conversions easier.
-   *rk on any radix of 2-10. Do not print any leading zeros. 
-   
+   *rk on any radix of 2-10. Do not print any leading zeros.
    */
 void printInt(int intToPrint, int radix) {
   int arraySize = 15;
@@ -103,13 +102,13 @@ void printInt(int intToPrint, int radix) {
   int temp;
   /* initialize with 0's, to cut out leading 0's and garbage */
   for ( i = 0; i < arraySize; i++ ) {
-    intArray[i] = 0; 
+    intArray[i] = 0;
   }
   for ( i = 1; i < arraySize; i++ ) {
     intArray[arraySize - i] = intToPrint % radix;
     temp = intToPrint / radix;
     if (temp == 0) {
-      break; 
+      break;
     }
     else {
       intToPrint = temp;
@@ -124,11 +123,11 @@ void printInt(int intToPrint, int radix) {
       }
     }
     if ( leadingZeroes == 1) {
-      printf("%d", intArray[j]); 
+      printf("%d", intArray[j]);
     }
   }
   printf("\n");
-  
+
 }
 
 /* Exit codes:
@@ -138,18 +137,21 @@ void printInt(int intToPrint, int radix) {
  */
 int main (int argc, char *argv[]){
   if (argc == 1) {
-    /* no arguements, do nothing */ 
+    /* no arguements, do nothing */
     return 0;
-  }  
+  }
   if (argc > 7) {
     printf("Maximum of 6 values accepted.  Quitting.\n");
     return 1;
-  } 
+  }
   printf("Values to convert: %d\n", argc - 1);
-  
+
   char *copyOfArgv[argc];
   int j;
-  printf("%c\n", *copyOfArgv[1]);
+  for ( j = 0; j < argc; j++ ) {
+    copyOfArgv[j] = argv[j];
+  }
+
   int intarray[argc];
   int i;
   for ( i = 1; i < argc; i++ ) {
