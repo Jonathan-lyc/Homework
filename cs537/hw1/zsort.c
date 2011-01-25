20efcd68ba215416a5b16adcae92c7ad9dafc6af
Josh <Josh@PCsForEducation.com>
2011-01-25 16:57:39 -0600

    Implemented a lot more code from generate.c that was provided by professor. Code does the same thing so far, but is much more compact.

--- cs537/hw1/zsort.c
+++ cs537/hw1/zsort.c
@@ -7,60 +7,41 @@
 #include "sort.h"
 
 void usage();
-void closeFiles();
 
 int main(int argc, char *argv[]) {
-  if (argc != 5) {
-    usage();
-  }
+  // The following code was inspired by generate.c :)
+
+  // args
   char input[50];
   char output[50];
 
-  int i = strcmp(argv[1], "-i");
-  int o = strcmp(argv[3], "-i");
-
-  if (i == 0) {
-    strcpy(input, argv[2]);
-    strcpy(output, argv[4]);
-  }
-  else if (o == 0) {
-    strcpy(input, argv[4]);
-    strcpy(output, argv[2]);
-  }
-  else {
-    usage();
-  }
-
-  //Check that input is readable and output is writable. This may be wrong..
-  if (access(input, R_OK) == -1 && access(output, W_OK) == -1 ) {
-    if (access(input, R_OK) != -1) {
-      fprintf(stderr, "Input file is not readable. Please check permissions.\n");
-      exit(1);
+  int c;
+  while((c = getopt(argc, argv, "i:o:")) != -1 {
+    switch (c) {
+    case 'i':
+      input = strdup(optarg);
+      break;
+    case 'o':
+      output = strdup(optarg);
+      break;
+    default:
+      usage();
     }
-    else {
-      fprintf(stderr, "Output file is not writable. Please check permissions.\n");
-      exit(1);
-    }
-
   }
 
-  //Get file pointers for input and output. Must now call close before
-  //any exit
-  FILE *inputfile;
-  FILE *outputfile;
-  inputfile = fopen(input, "r");
-  outputfile = fopen(output, "w");
-  if (inputfile == NULL) {
-    fprinf(stderr, "Input file %s is not readable. Please check permissions and that the file actually exists.\n", input);
-    closeFiles();
-    exit(1);
-  }
-  if (outputfile == NULL) {
-    fprintf(stderr, "Output file %s is not writable. Please check permissions.\n", output);
-    close();
-    exit(1);
+  // Attempt to open input file.
+  int input_fd = open(input, O_RONLY, S_IRWXU);
+  if (input_fd < 0) {
+    fprintf(stderr, "Error: Cannot open file %s", input);
   }
 
+  // Attempt to open output file.
+  int output_fd = open(output, O_WRONLY|O_CREAT|O_TRUNC, S_IRWXU);
+  if (output_fd < 0) {
+    fprintf(stderr, "Error: Cannot open file %s", output);
+  }
+
+  closeFiles(inputfile, input, outputfile, output);
   return(0);
 }
 
@@ -69,17 +50,3 @@ void usage() {
   exit(1);
 }
 
-void closeFiles() {
-  int i;
-  int o;
-  i = fclose(inputfile);
-  o = fclose(outputfile);
-  if (i != 0) {
-    fprintf(stderr, "Error closing input file %s", input);
-    exit(1);
-  }
-  if (o !- 0) {
-    fprintf(stderr, "Error closing output file %s", output);
-    exit(1);
-  }
-}
