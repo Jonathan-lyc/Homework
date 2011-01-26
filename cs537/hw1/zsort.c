f0cb9f7c54b429e975ce3284d9247afccaf54ab7
Josh Gachnang <Josh@PCsForEducation.com>
2011-01-26 12:53:19 -0600

    Got size of input and malloc that much space.

--- cs537/hw1/zsort.c
+++ cs537/hw1/zsort.c
@@ -11,15 +11,16 @@
 
 
 void usage();
+void fileError(char *file, int input_fd, int output_fd);
 
 int main(int argc, char *argv[]) {
   // The following code was inspired by generate.c :)
 
   // args
-  char *input;
-  char *output;
+  char *input = "";
+  char *output = "";
 
-  if (argc != 4) {
+  if (argc != 5) {
     usage();
   }
   int c;
@@ -39,17 +40,28 @@ int main(int argc, char *argv[]) {
   // Attempt to open input file.
   int input_fd = open(input, O_RDONLY, S_IRWXU);
   if (input_fd < 0) {
-    fprintf(stderr, "Error: Cannot open file %s", input);
+     fileError(input, input_fd, 0);
   }
 
   // Attempt to open output file.
   int output_fd = open(output, O_WRONLY|O_CREAT|O_TRUNC, S_IRWXU);
   if (output_fd < 0) {
-    fprintf(stderr, "Error: Cannot open file %s", output);
+    fileError(input, input_fd, output_fd);
   }
 
   // Get size of file for efficient malloc
+  struct stat buf;
+  int err = fstat(input_fd, &buf);
+  if (err != 0) {
+    fileError(input, input_fd, output_fd);
+  }
+  int size = buf.st_size;
+  printf("%d\n", size);
 
+  int *sort_array = malloc(size);
+  if (sort_array == NULL) {
+    fprintf(stderr, "Malloc failed");
+  }
   return(0);
 }
 
@@ -58,3 +70,12 @@ void usage() {
   exit(1);
 }
 
+void fileError(char *file, int input_fd, int output_fd) {
+  fprintf(stderr, "Error: Cannot open file %s\n", file);
+  //Errors at this point are irrelevant.
+  close(input_fd);
+  if (output_fd > 0) {
+    close(output_fd);
+  }
+  exit(1);
+}
