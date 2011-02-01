d9a2a5c08c06583d565bbbd4b241c3847086c5d1
Josh <Josh@PCsForEducation.com>
2011-02-01 16:58:13 -0600

    Started on the shell. Good framework, nothing really happening yet.

--- cs537/shell/mysh.c
+++ cs537/shell/mysh.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAXINPUT 256
+
+int prompt();
+void error();
+void batch();
 
 int
 main(int argc, char *argv[]){
-  printf("Hello World!");
+  if (argc > 2) {
+    error();
+  }
+  //Interactive mode
+  if (argc == 1) {
+    prompt();
+  }
+  if (argc == 2) {
+    batch();
+  }
+
+  prompt();
   return 0;
+}
+
+int
+prompt() {
+  char *input = malloc(MAXINPUT);
+  if (input == NULL) {
+    error();
+    return 1;
+  }
+
+  printf("mysh> ");
+  fgets (input, MAXINPUT, stdin);
+  if (input[strlen (input) -1] == '\n') {
+    input[strlen (input) - 1] = '\0';
+  }
+  printf("\nYou typed: %s\n", input);
+
+  //Get the command
+  int i;
+  return 0;
+}
+
+void
+error() {
+  printf("Error\n");
+  exit(1);
+}
+
+void
+batch() {
+  printf("Was batch mode fun?\n");
+  exit(1);
 }
