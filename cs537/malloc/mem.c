d912850e104375fe70977bd1a54b2cc0e8866116
PCsForEducation <Josh@PCsForEducation.com>
2011-03-21 22:01:15 -0500

    Added webserver base project.

--- cs537/malloc/mem.c
+++ cs537/malloc/mem.c
@@ -4,11 +4,18 @@
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+
 #include <fcntl.h>
 #include "mem.h"
 
-int dbg = -1;
-int merror;
+#define DEADBEEF (0xDEADBEEF)
+#define ABCDDCBA (0xABCDDCBA)
+#define HEADSIZE (16)
+
+int dbg = -1; //Debuging enabled/disabled/super mode and tracks init
+int mapsize; //Size of the mapped region
+int m_error; //Returns error codes
+int init = 0; //0 = not inited, 1=init occured.
 
 typedef struct __list_t {
   int size;
@@ -24,10 +31,16 @@ list_t *head;
 //free space. Point head to that node. Make sure head is always pointed
 //to first node. Just check if prev == NULL, if so, node is first.
 int Mem_Init(int sizeOfRegion, int debug) {
-  dbg = debug; //Debug mode
   
+  dbg = debug; //Debug mode
+  mapsize = sizeOfRegion;
+  if (init != 0) {
+	m_error = E_BAD_ARGS;
+	return -1;
+  }
+  init = 1; //Initialized
   if (dbg == -1 || sizeOfRegion < 1) {
-    merror = E_BAD_ARGS;
+    m_error = E_BAD_ARGS;
     return -1;
   }
 
@@ -39,7 +52,7 @@ int Mem_Init(int sizeOfRegion, int debug) {
     perror("mmap");
     return -1;
   }
-  
+  head = ptr;
   list_t *freespace;
   freespace = (list_t *) ptr;
 //   list_t freespace = {sizeOfRegion - sizeof(list_t), NULL, NULL, 0};
@@ -47,21 +60,24 @@ int Mem_Init(int sizeOfRegion, int debug) {
   freespace->next = NULL;
   freespace->prev = NULL;
   freespace->free = 0;
-  head = freespace;
   
-  printf("head:%p\n", head);
+  close(fd);
   return 0;
 }
 
 void *Mem_Alloc(int size) {
+  
   //Grab largest free, split up, add a new node, move largest ptr.
   
   //Align to 8 bytes for best performance
   if (size % 8 != 0) {
 	size = size + (size % 8);
   }
+  //Should check dbg between 0 & 2..Above 2 considered same as 0 I guess.
+  if (dbg == 3) {
+    printf("size:%d\n", size);
+  }
   
-  printf("size:%d\n", size);
   //Find largest free space (could be sped up, but this is easiest right now  
   list_t *largest = head;
   list_t *tmp = head;
@@ -71,10 +87,14 @@ void *Mem_Alloc(int size) {
 	}
 	tmp = tmp->next;
   }
+  if (largest->size < size + 32) {
+	m_error = E_NO_SPACE;
+	return NULL;
+  }
   
-  list_t *smaller = (list_t *) largest + 16 + size;
+  list_t *smaller = (list_t *) largest + HEADSIZE + size;
   
-  smaller->size = largest->size - 16 - size;
+  smaller->size = largest->size - HEADSIZE - size;
   smaller->next = largest->next;
   smaller->prev = largest;
   smaller->free = 0;
@@ -89,19 +109,72 @@ void *Mem_Alloc(int size) {
 // 	printf("%p\n", head);
 //   }
   
-  return largest + 16;
+  return largest + HEADSIZE;
 }
 
 int Mem_Free(void *ptr) {
+  if (ptr == NULL) {
+	return 0;
+  }
+  list_t *tofree = (list_t *) ptr - HEADSIZE;
+  if (tofree->size == 0 || tofree->free == 0) {
+	//Not a correct pointer
+	m_error = E_BAD_POINTER;
+	return -1;
+  }
+  tofree->free = 0;
+  //Coalesce
+  if (tofree->next != NULL) {
+	list_t *after = tofree->next;
+	if (after->free == 0) {
+	  //Block after tofree is free also. Coalesce.
+	  if (after->next != NULL) {
+		list_t *afterafter = after->next;
+		afterafter->prev = tofree;
+	  }
+	  tofree->size = tofree->size + HEADSIZE + after->size;
+	  tofree->next = after->next;
+	  
+	  
+// 	  list_t* before = tofree->prev;
+// 	  after->size = after->size + tofree->size + HEADSIZE;
+// 	  after->free = 0;
+// 	  after->prev = before;
+// 	  before->next = after;
+	}
+  }
+  if (tofree->prev != NULL) {
+	list_t *before = tofree->prev;
+	if (before->free == 0) {
+	  //Block before tofree is free also. Coalesce.
+	  list_t* after = tofree->next;
+	  before->size = before->size + tofree->size + HEADSIZE;
+	  before->free = 0;
+	  before->next = after;
+	  after->prev = before;
+	}
+  }
+  
   return 0;
 }
 
 void Mem_Dump() {
-  printf("dump:\n");
+  printf("List:\n");
   list_t *tmp = head;
   while (tmp) {
-    printf("  size:%d\n", tmp->size);
+    printf("  size:%d free:%d\n", tmp->size, tmp->free);
     tmp = tmp->next;
   }
+  tmp = head;
+  printf("Memory:\n");
+  
+//   int* i;   
+//   for(i = (int*) (head + 1); i < (int*) (head + 1) + mapsize / 8; i++) { 
+//     printf("%08x\n", (*i));
+//   }
+//   while (tmp < tmp + mapsize) {
+// 	printf("%d\n", *tmp);
+// 	tmp = tmp + 1;
+//   }
   return;
 }
