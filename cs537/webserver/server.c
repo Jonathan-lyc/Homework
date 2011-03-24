18a4984dc512c279b7863fac7e32e98592a80ce5
Josh <Josh@PCsForEducation.com>
2011-03-24 16:36:00 -0500

    Started making annotations and what not. Really really procrastinating here..

--- cs537/webserver/server.c
+++ cs537/webserver/server.c
@@ -1,5 +1,6 @@
 #include "cs537.h"
 #include "request.h"
+#include <string.h>
 
 // 
 // server.c: A very, very simple web server
@@ -12,40 +13,59 @@
 //
 
 // CS537: Parse the new arguments too
-void getargs(int *port, int argc, char *argv[])
+void usage() {
+    fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+    exit(1);
+}
+void getargs(int *port, int *threads, int *buffers, int *schedalg, int argc, char *argv[])
 {
-    if (argc != 2) {
-	fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-	exit(1);
-    }
+    if (argc != 2)
+        usage();
     *port = atoi(argv[1]);
+    if (port > 65535 || port < 0) {
+        usage();
+    }
+
+    *threads = atoi(argv[2]);
+    if (threads < 1)
+        usage();
+
+    *buffers = atoi(argv[3]);
+    if (buffers < 1)
+        usage();
+
+    // FIFO = 0, SFF = 1, SFF-BS = 2
+    if (strcmp(argv[4], "FIFO") == 0)
+        
 }
 
 
 int main(int argc, char *argv[])
 {
-    int listenfd, connfd, port, clientlen;
+    int listenfd, connfd, port, clientlen, threads, buffers, schedalg;
     struct sockaddr_in clientaddr;
 
-    getargs(&port, argc, argv);
+    getargs(&port, threads, buffers, schedalg, argc, argv);
 
     // 
-    // CS537: Create some threads...
+    // CS537: Create thread pool
     //
 
     listenfd = Open_listenfd(port);
+
+    // Add in check for the conditional variables here. This is the producer.
     while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
 
-	// 
-	// CS537: In general, don't handle the request in the main thread.
-	// Save the relevant info in a buffer and have one of the worker threads 
-	// do the work.
-	// 
-	requestHandle(connfd);
+        //
+        // CS537: In general, don't handle the request in the main thread.
+        // Save the relevant info in a buffer and have one of the worker threads
+        // do the work.
+        //
+        requestHandle(connfd);
 
-	Close(connfd);
+        Close(connfd);
     }
 
 }
